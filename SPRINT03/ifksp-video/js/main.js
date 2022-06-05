4b1599333d69213f1fb938381d27dbc4f0a76f82
SK <sulyok.kamilla@gmail.com>
2022-06-05 10:42:42 +0200

    changes

--- SPRINT03/ifksp-video/js/main.js
+++ SPRINT03/ifksp-video/js/main.js
@@ -2,21 +2,22 @@ console.log("Hello buddy, please tell me you can read this");
 function calcAmount() {
     let price = 1000;
     let amountInput = document.querySelector("input[name='amount-input']")
-    let showAmount = document.querySelector("span.show-amount");
+   
     let amountNumber = parseInt(amountInput.value);
 amountNumber = isNaN(amountNumber) ? 0 : amountNumber;
-    if (amountNumber > 10) {
-        alert("Maximum 10 termék rendelhető");
-
-    }
-    else if (amountNumber < 1)
-    { alert("Minimum 1 termék rendelhető"); }
-    else { 
-    let amount = amountNumber * price;
-    showAmount.innerHTML = amount;
-    }
-
+   
+showSumPrice (price, amountNumber);
+}
+function showSumPrice (price, amountNumber) {
+let showAmount = document.querySelector("span.show-amount");
+if (amountNumber > 10) {
+    alert("Maximum 10 termék rendelhető");
 
 }
-// Nevek
-let numberOfProducts;
+else if (amountNumber < 1)
+{ alert("Minimum 1 termék rendelhető"); }
+else { 
+let amount = amountNumber * price;
+showAmount.innerHTML = amount;
+}
+}
---
+++ SPRINT03/ifksp-video/js/scope.js
@@ -0,0 +1,6 @@
+// Global scope.
+const name2 = "Pisti";
+{ name1 = "Sanyi";
+console.log(name1);
+}
+console.log(name2);
